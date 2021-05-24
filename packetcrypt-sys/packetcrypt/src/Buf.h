394238e3518da364a90dd290741a17d7026117ca
huycuonglengoc <huycuonglengoc@gmail.com>
2021-05-24 17:08:16 +0700

    Add files via upload

--- packetcrypt-sys/packetcrypt/src/Buf.h
+++ packetcrypt-sys/packetcrypt/src/Buf.h
@@ -26,11 +26,11 @@ typedef union {
 } Buf32_t;
 _Static_assert(sizeof(Buf32_t) == 32, "buf16 size");
 typedef union {
-    Buf_TYPES(64);
-    Buf16_t sixteens[4];
+    Buf_TYPES(32);
+    Buf16_t sixteens[2];
     Buf32_t thirtytwos[2];
 } Buf64_t;
-_Static_assert(sizeof(Buf64_t) == 64, "buf64 size");
+_Static_assert(sizeof(Buf64_t) == 32, "buf64 size");
 
 #define Buf_IS_ARRAY(arg) __builtin_types_compatible_p( \
     __typeof__(arg), __typeof__((arg)[0]) [ ])
